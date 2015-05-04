d17b502d8a175bf598cf916613b7b2b63638d7c8
John Franchak <franchak@gmail.com>
2015-05-04 14:30:23 -0700

    fixed sync conflict

--- RunRecalibrationStudy.m
+++ RunRecalibrationStudy.m
@@ -9,25 +9,12 @@ stim_levels_nopack = 10:.5:55;
 
 [mu_est_pre, sigma_est_pre, trial_unit_pre, trial_resp_pre, time_pre] = DataCollection(out_dir, id{1}, 'pre', stim_levels_dec, 0, 30, 30);
 save(strcat(out_dir,'/',id{1}));
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 [mu_est_aff, sigma_est_aff, trial_unit_aff, trial_resp_aff, time_aff] = DataCollection(out_dir, id{1}, 'aff', stim_levels_aff, 4, 32 , 20);
 save(strcat(out_dir,'/',id{1}));
 [mu_est_pst, sigma_est_pst, trial_unit_pst, trial_resp_pst, time_pst] = DataCollection(out_dir, id{1}, 'pst', stim_levels_dec, 0, 30, 30);
 save(strcat(out_dir,'/',id{1}));
 [mu_est_npk, sigma_est_npk, trial_unit_npk, trial_resp_npk, time_npk] = DataCollection(out_dir, id{1}, 'npk', stim_levels_dec, 4, mu_est_aff - 10, 15);
-=======
->>>>>>> Stashed changes
-[mu_est_aff, sigma_est_aff, trial_unit_aff, trial_resp_aff, time_aff] = DataCollection(out_dir, id{1}, 'aff', stim_levels_aff, 4, 30, 20);
-save(strcat(out_dir,'/',id{1}));
-[mu_est_pst, sigma_est_pst, trial_unit_pst, trial_resp_pst, time_pst] = DataCollection(out_dir, id{1}, 'pst', stim_levels_dec, 0, 30, 30);
-save(strcat(out_dir,'/',id{1}));
-[mu_est_npk, sigma_est_npk, trial_unit_npk, trial_resp_npk, time_npk] = DataCollection(out_dir, id{1}, 'npk', stim_levels_dec, 4, mu_est_aff - 5, 15);
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/master
->>>>>>> Stashed changes
+
 save(strcat(out_dir,'/',id{1}));
 
 measurement_offset = inputdlg('Enter measurement camera reading with doorway fully closed')';
