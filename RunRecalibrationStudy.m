134c849308b182e20dc0dd1bc06c3a3d89a6cf6b
John Franchak <franchak@gmail.com>
2015-05-20 11:19:05 -0700

    Added between-subject conditions

--- RunRecalibrationStudy.m
+++ RunRecalibrationStudy.m
@@ -2,18 +2,19 @@ clear all
 tic
 out_dir = uigetdir('','Specify a directory to save output files');
 id = inputdlg('Enter participant id')';
+cond = inputdlg('Enter condition (f or b)');
 
 stim_levels_dec = 15:.5:60;
 stim_levels_aff = 15:.5:60;
 stim_levels_nopack = 10:.5:55;
 
-[mu_est_pre, sigma_est_pre, trial_unit_pre, trial_resp_pre, time_pre] = DataCollection(out_dir, id{1}, 'pre', stim_levels_dec, 0, 30, 30);
+[mu_est_pre, sigma_est_pre, trial_unit_pre, trial_resp_pre, time_pre] = DataCollection(out_dir, id{1}, strcat('pre_',cond{1}), stim_levels_dec, 0, 30, 30);
 save(strcat(out_dir,'/',id{1}));
-[mu_est_aff, sigma_est_aff, trial_unit_aff, trial_resp_aff, time_aff] = DataCollection(out_dir, id{1}, 'aff', stim_levels_aff, 4, 30, 20);
+[mu_est_aff, sigma_est_aff, trial_unit_aff, trial_resp_aff, time_aff] = DataCollection(out_dir, id{1}, strcat('aff_',cond{1}), stim_levels_aff, 4, 30, 20);
 save(strcat(out_dir,'/',id{1}));
-[mu_est_pst, sigma_est_pst, trial_unit_pst, trial_resp_pst, time_pst] = DataCollection(out_dir, id{1}, 'pst', stim_levels_dec, 0, 30, 30);
+[mu_est_pst, sigma_est_pst, trial_unit_pst, trial_resp_pst, time_pst] = DataCollection(out_dir, id{1}, strcat('pst_',cond{1}), stim_levels_dec, 0, 30, 30);
 save(strcat(out_dir,'/',id{1}));
-[mu_est_npk, sigma_est_npk, trial_unit_npk, trial_resp_npk, time_npk] = DataCollection(out_dir, id{1}, 'npk', stim_levels_dec, 4, mu_est_aff - 5, 15);
+[mu_est_npk, sigma_est_npk, trial_unit_npk, trial_resp_npk, time_npk] = DataCollection(out_dir, id{1}, strcat('npk_',cond{1}), stim_levels_dec, 4, mu_est_aff - 5, 10);
 save(strcat(out_dir,'/',id{1}));
 
 measurement_offset = inputdlg('Enter measurement camera reading with doorway fully closed')';
